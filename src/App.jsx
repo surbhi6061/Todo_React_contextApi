563e5d97da8de26f53c5046f137defbec81ec1ce
surbhi6061 <surbhikumari6061@gmail.com>
2024-04-16 13:29:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,8 @@ import { TodoProvider } from "./context";
 import { useEffect, useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
-import ReactGA from "react-ga4";
-const TRACKING_ID = "G-L82JGS1TF6";
-ReactGA.initialize(TRACKING_ID);
+
 function App() {
-  useEffect(() => {
-    ReactGA.send({hitType:"pageview",page:window.location.pathname + window.location.search});
-  }, []);
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ReactGA from "react-ga4";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-YM57NP3887");
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
