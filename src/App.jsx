09b9c30fcde58eaeab1ca0fd746dfda72985e529
surbhi6061 <surbhikumari6061@gmail.com>
2024-04-16 12:19:32 +0530

    analytics code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ const TRACKING_ID = "G-L82JGS1TF6";
 ReactGA.initialize(TRACKING_ID);
 function App() {
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+      title:"Home Page",
+    });
   }, []);
   const [todos, setTodos] = useState([]);
 
